94f120a870e493b403584187e2ed56c7640c1a3e
Junior743 <carloshenriquesousajunior@gmail.com>
2025-08-15 01:33:35 -0300

    Changed MONGO_URL

--- src/adapters/presentations/api/config/env.ts
+++ src/adapters/presentations/api/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   mongoUrl:
-    process.env.MONGO_URL || "mongodb://localhost:27017/padroes-projeto",
+    process.env.MONGO_URL || "mongodb://localhost:27017/clean-architecture",
   port: process.env.PORT || 3000,
 };
